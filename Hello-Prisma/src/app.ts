a596ffb0a747154594870337e199d04835785c56
Satayjit-Biswas <satayjitbiswas2@gmail.com>
2023-09-10 17:27:10 +0600

    Post CRUD , Category , and add Pagination

--- Hello-Prisma/src/app.ts
+++ Hello-Prisma/src/app.ts
@@ -1,5 +1,7 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import { CategoryRoutes } from "./modules/category/category.route";
+import { PostRoutes } from "./modules/post/post.route";
 import { UserRoutes } from "./modules/user/user.route";
 
 const router = express.Router();
@@ -9,6 +11,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 // All Routes
 app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1/category", CategoryRoutes);
+app.use("/api/v1/post", PostRoutes);
 
 // default Routes
 router.get("/", (req: Request, res: Response) => {
